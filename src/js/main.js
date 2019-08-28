10e74461e16acd4c7015a71a1ab24ed276a7daad
alina_ganich <alina.ganich@icloud.com>
2019-08-28 20:28:25 +0300

    header without changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
+    //Slider
+    $('.slider').slick({
+        infinity: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
 
-    $('#web').hide();
+    });
+
+    var header = $('#header'),
+        bannerHeight = $('#banner').innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    // Fixed header
+    checkScroll(scrollOffset);
+
+    $(window).on('scroll', function() {
+
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+        console.log(checkScroll);
+    })
+
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset >= bannerHeight) {
+            header.addClass('fixed');
+        } else {
+            header.removeClass('fixed');
+        }
+    }
 
 
 });
